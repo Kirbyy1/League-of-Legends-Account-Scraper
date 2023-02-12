6c1d463aec96406087b59362027231de43d2e8df
Kirbyy <81638158+Kirbyy1@users.noreply.github.com>
2023-02-12 10:40:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from requests import Response
 
 
 class DataRetriever:
-    URL = "https://www.leagueofgraphs.com/summoner/{region}/{display_name}/last-30-days"
+    URL: str = "https://www.leagueofgraphs.com/summoner/{region}/{display_name}/last-30-days"
     HEADERS: dict = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari"}
 
